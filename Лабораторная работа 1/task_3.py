e8507f6e7b299622af2ea4b50e23731ccb658ad6
regi1na <149325991+regi1na@users.noreply.github.com>
2023-10-29 21:37:06 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+half = len(list_players) // 2
+print(list_players[:half])
+print(list_players[half:])
